49fc797a8071a68c9b16bad89bec5b0ea0db9d22
Bjørn Winther <bwinth18@student.aau.dk>
2023-02-25 09:46:01 +0100

    Platform class

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    public bool solid;
-    public float timer;
+    private bool isSolid;
+    private float timer;
     public float timerThreshold = 1;
     Collider collider;
 
@@ -21,19 +21,58 @@ public class Platform : MonoBehaviour
 
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other) // can use courutine instead? - to wait x-time to execute.
     {
-        if (other.CompareTag("player"))
+        if (other.CompareTag("player")) //change this to "returnIF" but not the other code below)
         {
             timer += Time.deltaTime; // test if this works xD
             if (timer >= timerThreshold)
             {
-
+                if (isSolid)
+                {
+                    Success();
+                }
+                else
+                {
+                    GlassCracking();
+                    if (timer >= timerThreshold+0.5)
+                    {
+                        platformFall();
+                    }
+                }
+                
             }
         }
 
         //the following is the same as the above:
+        /*
         if (!other.CompareTag("player")) { return; }
         timer += Time.deltaTime;
+        if (timer <= timerThreshold) { return; }
+        */
     }
+
+    public void SetSolid(bool isSolid)
+    {
+        isSolid = this.isSolid;
+    }
+
+    public void GlassCracking()
+    {
+        //Sound?
+        //animation or material change.
+    }
+
+    public void platformFall()
+    {
+        //make platform fall (translate pos), and possibly delete?
+    }
+
+    public void Success()
+    {
+        //Sound
+        //light up perimiter of platform
+        //material change?
+    }
+
 }
