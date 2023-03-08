162043bbf35c247871b5c021836652f55dceb541
DonMiguel95 <ml.krarup@hotmail.com>
2023-03-08 14:53:57 +0100

    elevator stuff more

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
+
+
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        isPressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == presser)
+        {
+            button.transform.localPosition = new Vector3(0, 0.15f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+}
