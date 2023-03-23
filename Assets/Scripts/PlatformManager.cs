8d28a83e76dcf343b4a97022eaf0f0ac51e6c4e3
Bjørn Winther <bwinth18@student.aau.dk>
2023-03-23 14:37:00 +0100

    fda

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -75,7 +75,7 @@ public class PlatformManager : MonoBehaviour
                 //GameObject platform = platformArray[i, j];
                 platformArray[i,j] = Realtime.Instantiate("PlatformV2", new Vector3(transform.position.x + i * ColoumnMultiplier, 0, transform.position.z + j * RowMultiplier), Quaternion.identity, new Realtime.InstantiateOptions
                 {
-                    ownedByClient = false, // True? 
+                    ownedByClient = true, // True? 
                     preventOwnershipTakeover = false,
                     destroyWhenOwnerLeaves = false,
                     destroyWhenLastClientLeaves = true
