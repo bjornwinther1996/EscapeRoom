8a1db59ffaf408cbaea1e83ff177712f7137fe72
DonMiguel95 <ml.krarup@hotmail.com>
2023-05-01 14:48:42 +0200

    new levers

--- Assets/Scripts/LeverBehaviour.cs
+++ Assets/Scripts/LeverBehaviour.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 
 public class LeverBehaviour : MonoBehaviour
 {
+    public bool resetCondition = false;
+
+    // Change to be true when input by grabPressed is present
+    private bool isLeftGrabPressed = false;
+    private bool isRightGrabPressed = false;
 
     private static int leversPulledGlobal;
+    private LeverData syncedLeverData;
     
     private bool wasPulled = false;
     public bool IsReferenced;
 
     private GameObject elevator;
 
-    [SerializeField]
-    private LeverData syncedLeverData;
-
-    private ElevatorData syncedElevatorData;
-    
     private AudioSource audioSource;
     public AudioClip pulled;
 
@@ -38,51 +39,106 @@ public class LeverBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // MIGHT WORK DISABLED FOR TESTING
-        //syncedLeverData._leversPulled = leversPulledGlobal;
-
-
-        /*
-        if (!wasPulled && this.gameObject.name == "Lever_back(Clone)" && this.transform.rotation.eulerAngles.x > 110)
-        {
-            leversPulledGlobal += 1;
-            syncedLeverData._leversPulled = leversPulledGlobal;
-            Debug.Log("Amount of levers pulled = " + syncedLeverData._leversPulled);
-            wasPulled = true;
-            // Play Audio
-        }
-        else if (!wasPulled && this.gameObject.name == "Lever_front(Clone)" && this.transform.rotation.y < 0.45f)
-        {
-            leversPulledGlobal += 1;
-            syncedLeverData._leversPulled = leversPulledGlobal;
-            Debug.Log("Amount of levers pulled = " + syncedLeverData._leversPulled);
-            wasPulled = true;
-            // Play Audio
-        }
-        else if (!wasPulled && this.gameObject.name == "Lever_left(Clone)" && this.transform.localRotation.y < 0.45f)
-        {
-            leversPulledGlobal += 1;
-            syncedLeverData._leversPulled = leversPulledGlobal;
-            Debug.Log("Amount of levers pulled = " + syncedLeverData._leversPulled);
-            wasPulled = true;
-            // Play Audio
-        }
-        */
+        CheckForVRInput();
+        CheckForResetLever();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (!wasPulled && other.name == "ActivateTrigger")
+        if (!wasPulled && other.tag == "Hands" && isLeftGrabPressed || isRightGrabPressed)
         {
-            //leversPulledGlobal += 1;
-            //syncedLeverData._leversPulled = leversPulledGlobal;
+            if (this.gameObject.name == "Lever_front(Clone)")
+            {
+                this.transform.rotation = Quaternion.Euler(135, 180, 0);
+            }
+            else if (this.gameObject.name == "Lever_back(Clone)")
+            {
+                this.transform.rotation = Quaternion.Euler(135, 0, 0);
+            }
+            else if (this.gameObject.name == "Lever_left(Clone)")
+            {
+                this.transform.rotation = Quaternion.Euler(135, 90, 0);
+            }
+            else
+            {
+                this.transform.rotation = Quaternion.Euler(135, 270, 0); 
+            }
+
             syncedLeverData._leversPulled++;
-            Debug.Log("Amount of levers pulled = " + syncedLeverData._leversPulled);
             mat = meshRenderer.material;
             mat.SetColor("_EmissionColor", Color.green);
             audioSource.PlayOneShot(pulled, 0.7f);
             wasPulled = true;
+        }
+    }
 
+    private void CheckForVRInput()
+    {
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+
+        var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
+
+        if (leftHandDevices.Count == 1)
+        {
+            UnityEngine.XR.InputDevice device = leftHandDevices[0];
+
+            bool triggerValue;
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out triggerValue) && triggerValue)
+            {
+                isLeftGrabPressed = true;
+            }
+            else
+            {
+                isLeftGrabPressed = false;
+            }
+        }
+
+        if (rightHandDevices.Count == 1)
+        {
+            UnityEngine.XR.InputDevice device = rightHandDevices[0];
+
+            bool triggerValue;
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out triggerValue) && triggerValue)
+            {
+                isRightGrabPressed = true;
+            }
+            else
+            {
+                isRightGrabPressed = false;
+            }
+        }
+    }
+
+    private void CheckForResetLever()
+    {
+
+        if (resetCondition)
+        {
+
+            if (this.gameObject.name == "Lever_front(Clone)")
+            {
+                this.transform.rotation = Quaternion.Euler(45, 0, 0);
+            }
+            else if (this.gameObject.name == "Lever_back(Clone)")
+            {
+                this.transform.rotation = Quaternion.Euler(45, 0, 0);
+            }
+            else if (this.gameObject.name == "Lever_left(Clone)")
+            {
+                this.transform.rotation = Quaternion.Euler(45, 0, 0);
+            }
+            else
+            {
+                this.transform.rotation = Quaternion.Euler(45, 0, 0);
+            }
+
+            syncedLeverData._leversPulled--;
+            mat = meshRenderer.material;
+            mat.SetColor("_EmissionColor", Color.red);
+            wasPulled = false;
+            resetCondition = false;
         }
     }
 }
